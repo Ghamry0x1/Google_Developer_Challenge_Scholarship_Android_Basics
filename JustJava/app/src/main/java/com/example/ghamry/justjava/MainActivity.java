a3e2fc54031c50b74475a456460272f2260fa009
Mohamed El Ghamry <mohamed.elghamry97@gmail.com>
2017-12-03 21:50:39 +0200

    Updated JustJava App
    Finished Version

--- JustJava/app/src/main/java/com/example/ghamry/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/ghamry/justjava/MainActivity.java
@@ -1,67 +1,95 @@
 package com.example.ghamry.justjava;
 
-/**
- * IMPORTANT: Add your package below. Package name can be found in the project's AndroidManifest.xml file.
- * This is the package name our example uses:
- *
- * package com.example.android.justjava;
- *
- */
-
-
-import android.os.Bundle;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
 
-/**
- * This app displays an order form to order coffee.
- */
 public class MainActivity extends AppCompatActivity {
 
+    TextView quantityTextView;
+    TextView priceTextView;
+    CheckBox whippedCreamChk;
+    CheckBox chocolateChk;
+    EditText nameTxt;
+
     int quantity = 0;
+    int price = 0;
+    int topping = 0;
+    boolean hasWhippedCream;
+    boolean hasChocolate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    /**
-     * This method is called when the order button is clicked.
-     */
-    public void submitOrder(View view) {
-        String priceMessage = "Free";
-        displayMessage(priceMessage);
+        quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        priceTextView = (TextView) findViewById(R.id.price_text_view);
+        whippedCreamChk = (CheckBox) findViewById(R.id.whippedCreamChk);
+        chocolateChk = (CheckBox) findViewById(R.id.chocolateChk);
+        nameTxt = (EditText) findViewById(R.id.name);
     }
 
     public void increment(View view) {
         quantity++;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
     public void decrement(View view) {
-        if(quantity>0)
+        if (quantity > 0)
             quantity--;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
-    public void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+    public void displayQuantity(int quantity) {
+        quantityTextView.setText("" + quantity);
     }
 
-    /**
-     * This method displays the given quantity value on the screen.
-     */
-    private void display(int number) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
+    public void calculateTopping() {
+        hasWhippedCream = whippedCreamChk.isChecked();
+        hasChocolate = chocolateChk.isChecked();
+
+        if (hasWhippedCream)
+            topping++;
+        if (hasChocolate)
+            topping += 2;
     }
 
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    public int calculatePrice(View view) {
+        calculateTopping();
+        price = (5 + topping) * quantity;
+        return price;
     }
+
+    public void displayPrice(View view) {
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(calculatePrice(view)));
+        topping = 0;
+    }
+
+    public String printOrderDetails() {
+        return getString(R.string.name) + ": " + nameTxt.getText()
+                + "\n" + getString(R.string.quantity) + ": " + quantity
+                + "\n" + getString(R.string.added) + " " + getString(R.string.whippedCream) + ": " + hasWhippedCream
+                + "\n" + getString(R.string.added) + " " + getString(R.string.chocolate) + ": " + hasChocolate
+                + "\n\n" + getString(R.string.total) + ": " + priceTextView.getText()
+                + "\n" + getString(R.string.thankyou);
+    }
+
+    public void submitOrder(View view) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", "", null));
+        intent.putExtra(Intent.EXTRA_SUBJECT, "JustJava " + getString(R.string.orderFor) + " " + nameTxt.getText());
+        intent.putExtra(Intent.EXTRA_TEXT, printOrderDetails());
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
 }
